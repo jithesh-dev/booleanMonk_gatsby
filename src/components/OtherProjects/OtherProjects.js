7d3f689aeb45c956d98e558cf5d1019699700e8d
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-09-08 22:10:21 +0530

    404 Updated

--- src/components/OtherProjects/OtherProjects.js
+++ src/components/OtherProjects/OtherProjects.js
@@ -1,14 +1,15 @@
 import React from "react"
 import "./OtherProjects.css"
+import otherworks_00 from "../../assets/images/other_works/otherworks_00.jpg"
 import otherworks_01 from "../../assets/images/other_works/otherworks_01.jpg"
 import otherworks_02 from "../../assets/images/other_works/otherworks_02.jpg"
 import otherworks_03 from "../../assets/images/other_works/otherworks_03.jpg"
 import otherworks_04 from "../../assets/images/other_works/otherworks_04.jpg"
-import otherworks_05 from "../../assets/images/other_works/otherworks_05.png"
-import otherworks_06 from "../../assets/images/other_works/otherworks_06.png"
-import otherworks_07 from "../../assets/images/other_works/otherworks_07.png"
-import otherworks_08 from "../../assets/images/other_works/otherworks_08.png"
-import otherworks_09 from "../../assets/images/other_works/otherworks_09.png"
+// import otherworks_05 from "../../assets/images/other_works/otherworks_05.png"
+// import otherworks_06 from "../../assets/images/other_works/otherworks_06.png"
+// import otherworks_07 from "../../assets/images/other_works/otherworks_07.png"
+// import otherworks_08 from "../../assets/images/other_works/otherworks_08.png"
+// import otherworks_09 from "../../assets/images/other_works/otherworks_09.png"
 import { SRLWrapper } from "simple-react-lightbox"
 
 function OtherProjects() {
@@ -23,6 +24,8 @@ function OtherProjects() {
       <h5 className="section-headings">OTHER PROJECTS</h5>
       <SRLWrapper options={options}>
         <div className="project-container">
+          <img src={otherworks_00} alt="" className="op-tile" />
+
           <img src={otherworks_01} alt="" className="op-tile" />
 
           <img src={otherworks_02} alt="" className="op-tile" />
@@ -30,16 +33,6 @@ function OtherProjects() {
           <img src={otherworks_03} alt="" className="op-tile" />
 
           <img src={otherworks_04} alt="" className="op-tile" />
-
-          <img src={otherworks_05} alt="" className="op-tile" />
-
-          <img src={otherworks_06} alt="" className="op-tile" />
-
-          <img src={otherworks_07} alt="" className="op-tile" />
-
-          <img src={otherworks_08} alt="" className="op-tile" />
-
-          <img src={otherworks_09} alt="" className="op-tile" />
         </div>
       </SRLWrapper>
     </div>
