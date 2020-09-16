ba4cc6b914dfd2fded3b1cc6d48e7fb5d4c53c2e
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-09-16 12:03:08 +0530

    Text updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,19 +13,25 @@ function About() {
       <div className="a-content">
         <h5 className="section-headings">JITHESH KUMAR</h5>
         <p>
-          Hey, I'm Jithesh ,a Creative developer based in Kochi, Kerala,India
-          with a passion for interactive design and development, currently
-          working with the awesome people at Baobab Partners In the past, i have
-          had to work across multiple roles including Backend development,
-          Mobile development and Product design but lately, my interest has
-          gotten more to do with interactive design and front-end
-          implementation.
+          A Creative Freelance Full Stack Developer / Designer with 9+ years of
+          industry experince with a passion for interactive design and
+          development. I am currently based in Nottingham working on freelance
+          web projects.
         </p>
         <p>
-          Also a fan of landscape photography and have tried playing around with
-          mobile photography, some of which you can see here . I am available
-          for very interesting gigs, or talks about design, hip-hop, photography
-          or fooood
+          During my early years, I was working as a Senior Test Engineer (Team
+          Lead) for the biggest IT Company in India. Out of passion for coding,
+          I moved on to freelancing web projects and started my development
+          career in Laravel and Mysql. Since 2016, I have worked on various web
+          technologies like - React, Gatsby, Django, Node, MySQL, Postgres and
+          the list goes on. I have had to work across multiple roles including
+          Backend development, Front End development and UI/UX design but
+          lately, my interest has gotten more to do with interactive design and
+          front-end implementation specializing in React.
+        </p>
+        <p>
+          I am also a professional photographer specializing in weddings and
+          have tried playing around with portrait photography.
         </p>
       </div>
     </div>
